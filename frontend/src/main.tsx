90fb1cbfb93acd962205d7272783ba6939cbf0b8
Engon Ken Morel <engonken8@gmail.com>
2025-07-30 14:54:24 +0100

    Added w3 theme black, and tryed run

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
-import './style.css'
+import './w3pro.css'
+import './w3-theme-black.css'
 import App from './App'
 
-const container = document.getElementById('root')
-
-const root = createRoot(container!)
-
-root.render(
+createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>
